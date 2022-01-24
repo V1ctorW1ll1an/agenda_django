6ab21150e1eceae4a49e340b875d3129bb90dba8
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-01-24 20:25:11 -0300

    aprendendo sobre django admin

--- contatos/admin.py
+++ contatos/admin.py
@@ -1,3 +1,14 @@
+from re import search
+from typing import Container
 from django.contrib import admin
+from .models import Categoria, Contato
 
-# Register your models here.
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    list_filter = ('nome', 'sobrenome')
+    search_fields = ('nome',)
+    # ... entre outros filtros
+
+admin.site.register(Categoria)
+admin.site.register(Contato , ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Contato(models.Model):
     nome = models.CharField(max_length=255)
@@ -16,5 +19,8 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     # o que fazer quando uma categoria for deletada
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self) -> str:
+        return self.nome
 
 
