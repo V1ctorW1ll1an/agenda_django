8d0ae104ea31eadf865a1fa976d214c9ad3fdd09
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-01-25 20:35:48 -0300

    exibindo um unico contato

--- contatos/urls.py
+++ contatos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='', view=views.index, name='index')
+    path(route='', view=views.index, name='index'),
+    path(route='<int:contato_id>', view=views.ver_contato, name='ver_contato')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -6,3 +6,10 @@ def index(request):
     return render(request=request, template_name='contatos/index.html', context={
         'contatos': contatos
     })
+
+
+def ver_contato(request, contato_id):
+    contato_pelo_id = Contato.objects.get(id=contato_id)
+    return render(request=request, template_name='contatos/ver_contato.html', context={
+        'contato_pelo_id': contato_pelo_id
+    })
