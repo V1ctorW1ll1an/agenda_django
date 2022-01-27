fd45d8e655294d798f71be5ffa827b1f6b702703
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-01-26 21:00:22 -0300

    usando condicionais no django

--- contatos/admin.py
+++ contatos/admin.py
@@ -4,11 +4,12 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome','telefone', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     # list_filter = ('nome', 'sobrenome')
     search_fields = ('nome',)
     # ... entre outros filtros
+    list_editable= ('telefone', 'mostrar')
 
 admin.site.register(Categoria)
 admin.site.register(Contato , ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -19,6 +19,7 @@ class Contato(models.Model):
     descricao = models.TextField(blank=True)
     # o que fazer quando uma categoria for deletada
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    mostrar = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.nome
