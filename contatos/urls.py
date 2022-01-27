79cca3e5962c71cfaa5a902d2af620650fd57b45
Victor Willian de Sousa <victorwillian1012@gmail.com>
2022-01-26 21:42:27 -0300

    buscando nome ou sobrenome

--- contatos/urls.py
+++ contatos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path(route='', view=views.index, name='index'),
+    path(route='busca/', view=views.busca, name='busca'),
     path(route='<int:contato_id>', view=views.ver_contato, name='ver_contato')
 ]
--- contatos/views.py
+++ contatos/views.py
@@ -2,6 +2,7 @@ from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contato
 from django.core.paginator import Paginator
+from django.db.models import Q #para consultas mais complexas
 
 def index(request):
 
@@ -33,3 +34,17 @@ def ver_contato(request, contato_id):
     })
     # except Contato.DoesNotExist as e:
     #   raise Http404()
+
+
+def busca(request):
+    termo = request.GET.get('termo')
+    contatos = Contato.objects.order_by('nome').filter(
+        Q(nome__icontains =termo) | Q(sobrenome__icontains = termo)
+    )
+    print(contatos.query) # print na query de busca
+    paginator = Paginator(contatos, 1)
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+    return render(request=request, template_name='contatos/busca.html', context={
+        'contatos': contatos
+    })
